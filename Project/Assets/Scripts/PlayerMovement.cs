6945a8e07d2d6a49609abbcc447c051d9d4e4fd9
aminskey <aminskey@github.com>
2025-04-22 13:07:47 +0200

    Working on player prefab

--- Project/Assets/Scripts/PlayerMovement.cs
+++ Project/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float sensX, sensY;
     float xRot, yRot;
+    public float speed = 10f;
     Rigidbody rb;
+
+    [SerializeField] Transform center;
+    [SerializeField] float radius = 78f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask mask;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +29,18 @@ public class PlayerMovement : MonoBehaviour
         float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
 
         float horiz = Input.GetAxis("Horizontal"), vert = Input.GetAxis("Vertical");
-        rb.velocity = transform.forward * vert + transform.right * horiz;
+        Vector3 move = (transform.forward * vert + transform.right * horiz) * speed;
+        Vector3 toCenter = transform.position - center.position;
+        float yVel = rb.velocity.y;
+
+        if (Input.GetButton("Jump") && isGrounded()) {
+            yVel = 5f;
+        }
+
+        if (toCenter.magnitude > radius) { 
+            Vector3 clampedPos = center.position + toCenter.normalized * radius;
+            rb.MovePosition(clampedPos);
+        }
 
         yRot += mouseX;
         xRot -= mouseY;
@@ -31,5 +48,11 @@ public class PlayerMovement : MonoBehaviour
         xRot = Mathf.Clamp(xRot, -90f, 90f);
 
         transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+        rb.velocity = new Vector3(move.x, yVel, move.z);
+    }
+
+    bool isGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, 0.2f, mask);
     }
 }
